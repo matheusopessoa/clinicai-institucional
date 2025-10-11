fe6592462b6734ee2ff1a2597c626db203e4e9d6
Matheus Pessoa <matheuspessoa.tech@gmail.com>
2025-10-11 11:16:38 -0300

    fix url

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,12 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    historyApiFallback: true,
+  },
+  preview: {
+    port: 8080,
+    host: "::",
+    historyApiFallback: true,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
